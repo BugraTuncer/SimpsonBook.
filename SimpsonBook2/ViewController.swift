f3db85488bd70df6a8400d1c20baf7e349f92499
Buğra Tunçer <bugratuncer@bugra-macbook-air.local>
2018-10-26 20:38:06 +0300

    Initial Commit

---
+++ SimpsonBook2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook2
+//
+//  Created by Buğra Tunçer on 26.10.2018.
+//  Copyright © 2018 Buğra Tunçer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
