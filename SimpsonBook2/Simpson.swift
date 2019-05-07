fd4daa762c3ed16613a3079dc8d6b14dcdafaad0
Buğra Tunçer <>
2019-05-07 11:49:06 +0300

    first commit

---
+++ SimpsonBook2/Simpson.swift
@@ -0,0 +1,9 @@
+//
+//  Simpson.swift
+//  SimpsonBook2
+//
+//  Created by Buğra Tunçer on 26.10.2018.
+//  Copyright © 2018 Buğra Tunçer. All rights reserved.
+//
+
+import Foundation
---
+++ SimpsonBook2/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  SimpsonBook2
+//
+//  Created by Buğra Tunçer on 26.10.2018.
+//  Copyright © 2018 Buğra Tunçer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
